38452644a495c656ca561724f245b562a72879ae
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-07-14 11:53:48 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import love from "./love.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={love} className="App-logo" alt="logo" />
+        <h1>
+         Joshua T. Beets
+        </h1>
       </header>
     </div>
   );
